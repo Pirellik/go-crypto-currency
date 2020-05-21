e9a52ff130f0c43846eba25ad447d320d0db07e2
Paśko Kamil <kamil.pasko@nokia.com>
2020-05-21 22:34:15 +0200

    corsfix

--- logger/logger.go
+++ logger/logger.go
@@ -6,20 +6,9 @@ import (
 	"time"
 )
 
-func setupResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-}
-
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-
-		setupResponse(&w, r)
-		if (*r).Method == "OPTIONS" {
-			return
-		}
 
 		inner.ServeHTTP(w, r)
 
