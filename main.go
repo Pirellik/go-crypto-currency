52fc847697bed53bf520a3336bf0e7df9900c263
Paśko Kamil <kamil.pasko@nokia.com>
2020-05-21 22:48:05 +0200

    fix

--- main.go
+++ main.go
@@ -13,10 +13,11 @@ func main() {
 	address := os.Args[1]
 	router := currency.NewRouter(address)
 
+	allowedHeaders := handlers.AllowedHeaders([]string{"Accept", "Accept-Language", "Content-Type", "Content-Language", "Origin"})
 	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS", "PUT", "DELETE"})
 
 	// launch server
 	log.Fatal(http.ListenAndServe(":"+address,
-		handlers.CORS(allowedOrigins, allowedMethods)(router)))
+		handlers.CORS(allowedOrigins, allowedMethods, allowedHeaders)(router)))
 }
